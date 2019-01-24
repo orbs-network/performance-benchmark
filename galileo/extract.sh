05e4b36f4a81fc1c0c3b25375a5a787fdcf0352d
Kirill Maksimov <kirill@orbs.com>
2019-01-24 17:13:13 +0200

    Add extraction script

---
+++ galileo/extract.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export API_ENDPOINT=${API_ENDPOINT-http://localhost:8080}
+export COMMIT=${COMMIT-master}
+export RESULTS=results/${COMMIT}/$(date +%Y-%m-%dT%H:%M:%SZ)
+
+mkdir -p $RESULTS
+
+for profile in block goroutine heap mutex threadcreate; do
+    curl -sK -v ${API_ENDPOINT}/debug/pprof/${profile} > ${RESULTS}/${profile}.out
+done
+
+curl -sK -v ${API_ENDPOINT}/metrics > ${RESULTS}/metrics.json
