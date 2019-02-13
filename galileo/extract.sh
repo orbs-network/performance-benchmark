de876ad6a92e89b37fad0a059e2b5931eb17323d
Kirill Maksimov <kirill@orbs.com>
2019-02-13 11:58:37 +0200

    Update extract.sh (breaks galileo)

--- galileo/extract.sh
+++ galileo/extract.sh
@@ -1,17 +1,18 @@
 #!/bin/bash -xe
 
 export API_ENDPOINT=${API_ENDPOINT-http://localhost:8080}
+export BASE_URL=${BASE_URL-http://localhost:8080}
 export COMMIT=${COMMIT-master}
-export RESULTS=results/${COMMIT}/$(date +%Y-%m-%d-%H%M%S)
+export RESULTS=${RESULTS-results/${COMMIT}/$(date +%Y-%m-%d-%H%M%S)}
 
 go test ./../benchmark/...
 
 mkdir -p $RESULTS
 
 for profile in block goroutine heap mutex threadcreate; do
-    curl -sK --connect-timeout 2s -v ${API_ENDPOINT}/debug/pprof/${profile} > ${RESULTS}/${profile}.out
+    curl -sK --connect-timeout 2s -v ${BASE_URL}/debug/pprof/${profile} > ${RESULTS}/${profile}.out
 done
 
-curl -sK --connect-timeout 2s -v ${API_ENDPOINT}/metrics > ${RESULTS}/metrics.json
+curl -sK --connect-timeout 2s -v ${BASE_URL}/metrics > ${RESULTS}/metrics.json
 
 echo $RESULTS
