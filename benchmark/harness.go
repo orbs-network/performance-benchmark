82377ca062203d012a59a4a187dcfa191245bddb
Ido Zilberberg <idoz@orbs.com>
2019-02-19 09:23:20 +0200

    Log every node

--- benchmark/harness.go
+++ benchmark/harness.go
@@ -10,41 +10,19 @@ import (
 	"github.com/stretchr/testify/require"
 	"io/ioutil"
 	"net/http"
-	"os"
-	"strconv"
-	"strings"
 	"testing"
 	"time"
 )
 
-type E2EConfig struct {
-	vchainId         uint32
-	baseUrl          string
-	ethereumEndpoint string
-
-	StressTestConfig
-}
-
-type StressTestConfig struct {
-	numberOfTransactions  uint64
-	acceptableFailureRate uint64
-	targetTPS             float64
-	txPerMin              float64
-	metricsEveryNth       uint64
-}
-
-const VIRTUAL_CHAIN_ID = uint32(42)
-
-// "github.com/orbs-network/orbs-spec/types/go/protocol"
-const PROCESSOR_TYPE_NATIVE = 1
-
 type harness struct {
+	config *E2EConfig
 	client *orbsClient.OrbsClient
 }
 
-func newHarness(vchainId uint32) *harness {
+func newHarness(config *E2EConfig) *harness {
 	return &harness{
-		client: orbsClient.NewClient(getConfig().baseUrl, vchainId, codec.NETWORK_TYPE_TEST_NET),
+		config: config,
+		client: orbsClient.NewClient(getConfig().baseUrl, config.vchainId, codec.NETWORK_TYPE_TEST_NET),
 	}
 }
 
@@ -106,13 +84,24 @@ func (h *harness) absoluteUrlFor(endpoint string) string {
 	return getConfig().baseUrl + endpoint
 }
 
+func (h *harness) metricsEndpoint(ip string) string {
+	vchainIdStr := fmt.Sprintf("%d", h.config.vchainId)
+	return "http://" + ip + "/vchains/" + vchainIdStr + "/metrics"
+}
+
 type metrics map[string]map[string]interface{}
 
-func (h *harness) getMetrics() metrics {
-	res, err := http.Get(h.absoluteUrlFor("/metrics"))
+func (h *harness) getMetricsFromMainNode() metrics {
+	return h.getMetrics(h.absoluteUrlFor("/metrics"))
+}
+
+func (h *harness) getMetrics(metricsUrl string) metrics {
+
+	res, err := http.Get(metricsUrl)
 
 	if err != nil {
-		fmt.Println(h.absoluteUrlFor("/metrics"), err)
+		fmt.Printf("Failed to read metrics from %s: %s\n", metricsUrl, err.Error())
+		return nil
 	}
 
 	if res == nil {
@@ -128,7 +117,7 @@ func (h *harness) getMetrics() metrics {
 
 func (h *harness) waitUntilTransactionPoolIsReady(t *testing.T) {
 	require.True(t, Eventually(3*time.Second, func() bool { // 3 seconds to avoid jitter but it really shouldn't take that long
-		m := h.getMetrics()
+		m := h.getMetricsFromMainNode()
 		if m == nil {
 			return false
 		}
@@ -142,60 +131,4 @@ func (h *harness) waitUntilTransactionPoolIsReady(t *testing.T) {
 func printTestTime(t *testing.T, msg string, last *time.Time) {
 	t.Logf("%s (+%.3fs)", msg, time.Since(*last).Seconds())
 	*last = time.Now()
-}
-
-func getConfig() *E2EConfig {
-	vchainId := VIRTUAL_CHAIN_ID
-	baseUrl := "http://localhost:8080"
-
-	stressTestNumberOfTransactions := uint64(1000)
-	stressTestTransactionsPerMinute := float64(240)
-	stressTestFailureRate := uint64(20)
-	stressTestTargetTPS := float64(20)
-	stressTestMetricsEveryNthTransaction := uint64(10)
-
-	ethereumEndpoint := "http://127.0.0.1:8545"
-
-	if os.Getenv("API_ENDPOINT") != "" {
-		apiEndpoint := os.Getenv("API_ENDPOINT")
-		baseUrl = strings.TrimRight(strings.TrimRight(apiEndpoint, "/"), "/api/v1")
-		ethereumEndpoint = os.Getenv("ETHEREUM_ENDPOINT")
-	}
-
-	if vcid, err := strconv.ParseUint(os.Getenv("VCHAIN"), 10, 0); err == nil {
-		vchainId = uint32(vcid)
-	}
-
-	if numTx, err := strconv.ParseUint(os.Getenv("STRESS_TEST_NUMBER_OF_TRANSACTIONS"), 10, 0); err == nil {
-		stressTestNumberOfTransactions = numTx
-	}
-
-	if txPerMin, err := strconv.ParseUint(os.Getenv("STRESS_TEST_TRANSACTIONS_PER_MINUTE"), 10, 0); err == nil {
-		stressTestTransactionsPerMinute = float64(txPerMin)
-	}
-
-	if metricsEveryNth, err := strconv.ParseUint(os.Getenv("STRESS_TEST_METRICS_EVERY_NTH_TRANSACTION"), 10, 0); err == nil {
-		stressTestMetricsEveryNthTransaction = uint64(metricsEveryNth)
-	}
-	if failRate, err := strconv.ParseUint(os.Getenv("STRESS_TEST_FAILURE_RATE"), 10, 0); err == nil {
-		stressTestFailureRate = failRate
-	}
-
-	if tps, err := strconv.ParseFloat(os.Getenv("STRESS_TEST_TARGET_TPS"), 0); err == nil {
-		stressTestTargetTPS = tps
-	}
-
-	return &E2EConfig{
-		vchainId,
-		baseUrl,
-		ethereumEndpoint,
-
-		StressTestConfig{
-			numberOfTransactions:  stressTestNumberOfTransactions,
-			acceptableFailureRate: stressTestFailureRate,
-			targetTPS:             stressTestTargetTPS,
-			txPerMin:              stressTestTransactionsPerMinute,
-			metricsEveryNth:       stressTestMetricsEveryNthTransaction,
-		},
-	}
 }
